01b925fb36a4adfacdffd411eb7c08e8cff75076
marksikaundi <emmanuelmark117@gmail.com>
2022-05-19 18:11:31 +0200

    added demo

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear() {
-
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
 
     delete() {
@@ -13,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        this.currentOperand = number
     }
 
     chooseOperation(operation) {
@@ -25,10 +28,9 @@ class Calculator {
     }
 
     updateDisplay() {
-        
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
-
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -39,8 +41,14 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
 
 
